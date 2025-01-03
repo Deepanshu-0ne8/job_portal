507ec6a4d2ff0a77a5b8979673ad07775f41af37
Deepanshu-0ne8 <patidardeepanshu910@gmail.com>
2025-01-03 18:37:42 +0530

    job page upto description

--- src/pages/job.jsx
+++ src/pages/job.jsx
@@ -1,7 +1,11 @@
+import { useUser } from '@clerk/clerk-react'
 import React from 'react'
 
 const JobPage
  = () => {
+
+  const { isLoaded, user } = useUser();
+
   return (
     <div>JobPage
     </div>
